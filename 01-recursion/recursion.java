eb97530514048001af922e5ff0f82a20cf81c0f4
Shi Shu <shishu_1998@yahoo.com>
2015-02-06 09:25:34 -0500

    Recursion

--- 01-recursion/recursion.java
+++ 01-recursion/recursion.java
@@ -42,7 +42,18 @@ public class recursion{
 	}
     }
 
-    
+    public int bunnyEars2(int bunnies) {
+	if(bunnies == 0){
+	    return 0;
+	}
+	else if(bunnies % 2 == 0){
+	    return 3 + bunnyEars2(bunnies - 1);
+	}
+	else{
+	    return 2 + bunnyEars2(bunnies - 1);
+	}
+    }
+
     public static void main(String[] args){
 	recursion a = new recursion();
 	System.out.println(a.factorial(3));
