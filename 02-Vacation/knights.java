1a635ef263139eaa5b068eb191fcee0c6a54bd93
shishu1998 <shishu_1998@yahoo.com>
2015-02-19 14:28:57 -0500

    Algorithm

--- 02-Vacation/knights.java
+++ 02-Vacation/knights.java
@@ -1,26 +1,90 @@
+import java.io.*;
+import java.util.*;
+
 public class knights{
+    private boolean done = false;
+    private int me = 9;
     private int moves = 0;
+    private int visited = -2;
     private int[][] board;
-    private int[][] test;
 
     public String toString(){
-	tour(test);
+	String s = "";
 	for(int i = 0;i < board.length; i ++){
 	    for(int b = 0; b < board[0].length; b ++){
-		System.out.println(board[i][b]);
+	        s = s + String.format("%d",(board[i][b])) + " ";
 	    }
+	    s = s + "\n";
 	}
+	return s;
     }
-    public void tour(int[][] input){
-	int[][] board = new int[input.length + 4][input[0].length + 4];
+    public void create(){
+	board = new int[9][9];
         for(int i = 0; i < board[0].length; i ++){
 	    board[0][i] = -1;
 	    board[1][i] = -1;
 	    board[board.length - 2][i] = -1;
 	    board[board.length - 1][i] = -1;
 	}
+	for(int i = 1; i < board[0].length - 1; i ++){
+	    board[i][0] = -1;
+	    board[i][1] = -1;
+	    board[i][board.length - 2] = -1;
+	    board[i][board.length - 1] = -1;
+	    
+	}
 	
 
+    }
+    
+    public boolean check(){
+	for(int i = 0; i < board.length; i ++){
+	    for(int b = 0;b <board[0].length; b ++){
+		if(board[i][b] != me  || board[i][b] != visited ){
+		    return false;
+		}
+	    }
+	}
+	return true;
+    }
+    
+    public void delay(int n){
+	try {
+	    Thread.sleep(n);
+	} catch (Exception e) {}
+    }
+    
+    public void tour(int x, int y){
+	if(board[x][y] == -1 || board[x][y] == me || board[x][y] == visited || done){
+	    return;
+	}
+	if(check()){
+	    done = true;
+	    System.out.println(this);
+	}
+	delay(100);
+	System.out.println(this);
+	board[x][y] = me;
+        tour(x + 1, y + 2);
+        tour(x - 1, y + 2);
+        tour(x + 1, y - 2);
+        tour(x - 1, y - 2);
+        tour(x + 2, y + 1);
+	tour(x + 2, y - 1);
+	tour(x - 2, y + 1);
+	tour(x - 2, y - 1);
+	if(!done){
+	    board[x][y] = visited;
+	}
+    }
+    
+    public static void main(String[] args){
+	knights a  = new knights();
+	a.create();
+	System.out.println(a);
+	a.tour(2,2);
+	System.out.println(a);
+	
     }
 
 
