a7723720b2b253fdaf6f8d467de2d48fbca88036
Shi Shu <shishu_1998@yahoo.com>
2015-02-23 09:25:33 -0500

    In progress

--- 03-Lab/makelake.java
+++ 03-Lab/makelake.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Lake{
     Random r = new random();
     private int[][] lake;
-    private int[] dig;
+    private int[] initial;
     public void makelake(int row, int column, int Elevation, int dig){
 	for(int i = 0; i < row; i ++){
-	    int[] dig = new int[column];
+	    int[] initial = new int[column];
 	    for(int b = 0; b < column ; b ++){
-		dig[b] = r.nextInt(5000) + 1;
+		initial[b] = r.nextInt(dig) + 1;
 	    }
-	    int[i] = dig;
+	    int[i] = initial;
 	}
     }
 }
