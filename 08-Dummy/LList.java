1e965f5f94a18955d6d65420952a6927f37ff553
shishu1998 <shishu_1998@yahoo.com>
2015-03-17 18:30:41 -0400

    New methods added

--- 08-Dummy/LList.java
+++ 08-Dummy/LList.java
@@ -1,37 +1,47 @@
 public class LList{
     private Node l=null;
-    private int length = 0;
+    private Node dum;
+    private int len = 0;
+
     public void add(String s){
-	l.setData(s);
-	Node dum = new Node();
+	dum = new Node("");
+        Node temp = new Node(s);
+	temp.setNext(l);
+	l = temp;
 	dum.setNext(l);
-	l = dum;
-	length ++;
+	len ++;
     }
     public String toString(){
 	String s = "";
 	Node tmp;;
-	for (tmp=l ; tmp!=null ; tmp=tmp.getNext()){
+	for (tmp= dum ; tmp!=null ; tmp=tmp.getNext()){
 	    s = s + tmp + " --> ";
 	}
 	s = s + "null";
 	return s;
     }
-    public String find(int n){
-	Node temp = l;
-	for(int i = 0;i < n; i ++){
+    public Node get(int n){
+	Node temp = dum;
+	for(int i = 0;i < n + 1; i ++){
 	    temp = temp.getNext();
 	}
-	return temp.getData();	
+	return temp;	
     }
+
+    public void remove(int index){
+	Node front = get(index - 1);
+	Node back = get(index +1);
+	front.setNext(back);
+    }
+    
     public void insert(int index, String s){
 	Node a = new Node(s);
-	Node before = l;
-	for(int i = 0; i < index - 1;i ++){
+	Node before = dum;
+	for(int i = 0; i < index;i ++){
 	    before = before.getNext();
 	}
-	Node after = l;
-	for(int i = 0; i < index; i ++){
+	Node after = dum;
+	for(int i = 0; i < index + 1; i ++){
 	    after = after.getNext();
 	}
 	a.setNext(after);
