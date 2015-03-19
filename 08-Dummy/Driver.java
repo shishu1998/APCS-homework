b368c626be708be64a39d0c1490963225b949706
shishu1998 <shishu_1998@yahoo.com>
2015-03-18 21:09:27 -0400

    Timing added

--- 08-Dummy/Driver.java
+++ 08-Dummy/Driver.java
@@ -3,18 +3,18 @@ import java.util.*;
 public class Driver{
     public static void main(String[] args) {
 	int sum;
-	long start,elapsed;
+	long start;
 	double diff;
 	LList l = new LList();
-	for (int i=0;i<1000;i++){
+	for (int i=0;i<10000;i++){
 	    l.add(i);
 	}
         ArrayList<Integer> al = new ArrayList<Integer>();
-	for(int i = 0; i < 1000; i ++){
+	for(int i = 0; i < 10000; i ++){
 	    al.add(i);
 	}
 	LinkedList<Integer> a = new LinkedList<Integer>();
-	for(int i = 0; i < 1000; i ++){
+	for(int i = 0; i < 10000; i ++){
 	    a.add(i);
 	}
 	start = System.currentTimeMillis();
@@ -23,7 +23,7 @@ public class Driver{
 	    sum = sum + l.get(i);
 	}
 	System.out.println("" + sum);
-	elapsed = System.currentTimeMillis();
+	System.out.println(System.currentTimeMillis()-start);
 
 	start = System.currentTimeMillis();
 	sum = 0;
@@ -31,15 +31,15 @@ public class Driver{
 	    sum = sum + l.get(i);
 	}
 	System.out.println("" + sum);
-	elapsed = System.currentTimeMillis();
+	System.out.println(System.currentTimeMillis()-start);
 
 	start = System.currentTimeMillis();
 	sum = 0;
 	for(int i = 0;i < a.size(); i ++){
 	    sum = sum + l.get(i);
 	}
-	elapsed = System.currentTimeMillis();
 	System.out.println("" + sum);
+	System.out.println(System.currentTimeMillis()-start);
 
 	
     }
