f5063a0222ad03120fce6b9792d6b276cc8f1ef7
Shi Shu <shishu_1998@yahoo.com>
2015-03-24 09:25:35 -0400

    Attempt at array

--- 10-Stacks/Driver.java
+++ 10-Stacks/Driver.java
@@ -1,5 +1,6 @@
 public class Driver{
     public static void main(String[] args){
+	/*
 	myStack<Integer> a = new myStack<Integer>();
 	a.push(8);
 	a.push(3);
@@ -8,6 +9,16 @@ public class Driver{
 	System.out.println(a.pop());
 	System.out.println(a);
 	System.out.println(a.empty());
-	System.out.println(a.top());
+	System.out.println(a.top());*/
+	myStack2<Integer> b = new myStack2<Integer>();
+	b.push(8);
+	b.push(3);
+	b.push(5);
+	System.out.println(b);
+	System.out.println(b.pop());
+	System.out.println(b.pop());
+	System.out.println(b.pop());
+	System.out.println(b);
+	
     }
 }
