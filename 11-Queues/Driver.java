f26c5ecc53bce0ed953b3690421ba359f5f10e28
Shi Shu <shishu_1998@yahoo.com>
2015-03-26 09:25:05 -0400

    Arrays work cept Head()

--- 11-Queues/Driver.java
+++ 11-Queues/Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String[] args){
-	myQueue<Integer> a = new myQueue<Integer>();
+	/*myQueue<Integer> a = new myQueue<Integer>();
 	System.out.println(a.empty());
 	a.enqueue(10);
 	a.enqueue(9);
@@ -10,7 +10,24 @@ public class Driver{
 	System.out.println(a.dequeue());
 	System.out.println(a);
 	System.out.println(a.dequeue());
-	System.out.println(a);
+	System.out.println(a);*/
+	myQueue2<Integer> b = new myQueue2<Integer>();
+	System.out.println(b.empty());
+	b.enqueue(10);
+	b.enqueue(9);
+	b.enqueue(5);
+	b.enqueue(8);
+	b.enqueue(7);
+	System.out.println(b);
+	System.out.println(b.dequeue());
+	System.out.println(b.dequeue());
+	System.out.println(b.dequeue());
+	System.out.println(b.dequeue());
+	System.out.println(b.dequeue());
+	System.out.println(b.dequeue());
+	System.out.println(b.dequeue());
+	System.out.println(b);
+	System.out.println(b.head());
 	
     }
 }
