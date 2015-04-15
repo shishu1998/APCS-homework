40fa8d4c3f4e5a603839d9d802df41a634fad352
Shi Shu <shishu_1998@yahoo.com>
2015-04-15 09:24:09 -0400

    Started Euclidean

--- 13-BFS/Node.java
+++ 13-BFS/Node.java
@@ -1,13 +1,15 @@
-
+import java.util.*;
 public class Node {
     private int x,y;
     private Node prev;
     private int priority;
+    //private double priority;
 		
     public Node(int x, int y){
 	this.x = x;
 	this.y = y;
 	priority = 26 -x + 18- y; 
+	//priority = Math.sqrt((26-x) *(26-x) + (18-y) * (18-y));
     }
 
     public Node getPrev() {
