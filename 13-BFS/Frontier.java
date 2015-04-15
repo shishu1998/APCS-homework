b977c7ed8d316ee1f8939abcff063a91189a8fa6
Shi Shu <shishu_1998@yahoo.com>
2015-04-15 09:17:11 -0400

    classwork, hard coded

--- 13-BFS/Frontier.java
+++ 13-BFS/Frontier.java
@@ -1,19 +1,42 @@
 import java.util.*;
 
 public class Frontier {
-		public LinkedList<Node> l = new LinkedList<Node>();
+    public LinkedList<Node> l = new LinkedList<Node>();
 
-		public void add(Node n){
-				l.add(n);
-		}
+    public void add(Node n){
+	l.add(n);
+    }
+    public Node getHigh(){
+	Node temp = l.get(0);
+	Node output = temp;
+	int index = 0;
+	while(index < l.size()){
+	    temp = l.get(index);
+	    index ++;
+	    if(temp.getPri() <= output.getPri()){
+		output = temp;
+	    }
+	}
+	return output;
+	
+    }
 
-		public Node remove() {
-				return l.remove(0);
-		}
+    public Node remove() {
+	int index = 0;
+	Node temp = l.get(0);
+	while(index<l.size()){
+	    if(temp == getHigh()){
+		break;
+	    }
+	    index ++;
+	    temp = l.get(index);
+	}
+	return l.remove(index);
+    }
 
-		public boolean isEmpty(){
-				return l.isEmpty();
-		}
+    public boolean isEmpty(){
+	return l.isEmpty();
+    }
 }
 
 
--- 13-BFS/Node.java
+++ 13-BFS/Node.java
@@ -1,28 +1,34 @@
 
 public class Node {
-		private int x,y;
-		private Node prev;
+    private int x,y;
+    private Node prev;
+    private int priority;
 		
-		public Node(int x, int y){
-				this.x = x;
-				this.y = y;
-		}
+    public Node(int x, int y){
+	this.x = x;
+	this.y = y;
+	priority = 26 -x + 18- y; 
+    }
 
-		public Node getPrev() {
-				return prev;
-		}
-
-		public void setPrev(Node n){
-				prev = n;
-		}
+    public Node getPrev() {
+	return prev;
+    }
+    
+    public int getPri(){
+	return priority;
+    }
+    
+    public void setPrev(Node n){
+	prev = n;
+    }
 		
-		public int getX() {
-				return x;
-		}
+    public int getX() {
+	return x;
+    }
 
-		public int getY() {
-				return y;
-		}
+    public int getY() {
+	return y;
+    }
 		
 }
 
